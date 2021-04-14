72007e69b3191f5719dffae1bca1156cd6efe0fc
Guillermo <rollerguille@outlook.es>
2021-04-14 19:49:20 +0200

    Form example with correct redirects
    Basic form with some redirections.

--- src/main/java/com/project/webDemo/controller/GreetingController.java
+++ src/main/java/com/project/webDemo/controller/GreetingController.java
@@ -1,17 +1,33 @@
 package com.project.webDemo.controller;
 
+import com.project.webDemo.entity.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Controller
 public class GreetingController {
 
+    Logger log = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        log.info("Get Request");
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        log.info("Post Request -> " + greeting.toString());
+        model.addAttribute("greeting", greeting);
+        return "result";
     }
 
 }
